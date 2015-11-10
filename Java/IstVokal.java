70c90f53df8a08dfb1332f5ea2e28710a3deefc0
Back2RL <leonardoertelt@web.de>
2015-11-10 11:28:53 +0100

    added IstVokal.java

---
+++ Java/IstVokal.java
@@ -0,0 +1,32 @@
+/**
+ *	Di 10. Nov 11:05:51 CET 2015
+ *
+ *	PR1, WS2015/16
+ *
+ *	Leonard Oertelt
+ *	Matrikelnummer 1276156
+ *	leonard.oertelt@stud.hs-hannover.de
+ * 
+ *	Julian Opitz
+ * 	Matrikelnummer 1302082
+ * 	julian.opitz@stud.hs-hannover.de
+ *
+ *	-----------------------------------------
+ *	Programmbeschreibung
+ */
+
+public class IstVokal {
+	public static void main(String[] args) {
+char c = 'a';	
+
+		if ("aeiou".indexOf(c) >= 0) {
+			System.out.println("Es ist ein Vokal.");
+		}
+		else {
+			System.out.println("Es ist kein Vokal.");
+
+		}
+
+
+	}
+}
