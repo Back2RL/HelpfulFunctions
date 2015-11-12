2b974891e03aaa742afe0b29875ee620a500ec52
Back2RL <leonardoertelt@web.de>
2015-11-12 11:08:54 +0100

    added Java primenumber

---
+++ Java/Prim.java
@@ -0,0 +1,51 @@
+/**
+ *	Do 12. Nov 11:06:50 CET 2015
+ *
+ *	PR1, WS2015/16
+ *
+ *	Leonard Oertelt
+ *	Matrikelnummer 1276156
+ *	leonard.oertelt@stud.hs-hannover.de
+ *	-----------------------------------------
+ *	calculates primenumbers from 2 - n (n is userinput)
+ */
+import java.util.*;
+
+public class Prim {
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		System.out.print("Your number: ");
+		int number = console.nextInt();
+		if (number <= 1) {
+			System.out.println("This number is too small.");
+		}
+		else {
+			System.out.print(2);
+			int cnt = 1;
+			for(int i = 3; i <= number; i++) {
+				if(countFactors(i) == 2) {
+					cnt++;
+					System.out.print(", " + i);
+				}
+			}
+		}
+	}
+
+	// is the number a primenumber	
+	public static boolean isPrime(int n) {
+		return (n > 1 && countFactors(n) == 2);
+	}
+
+	// returns the number of factors a number has
+	public static int countFactors(int n) {
+		return n;
+	}
+
+	// returns the next primenumber or n if n is primenumber
+	public static int nextPrime(int n) {
+		while (!isPrime(n)) {
+			n++;
+		}
+		return n;
+	}
+}
