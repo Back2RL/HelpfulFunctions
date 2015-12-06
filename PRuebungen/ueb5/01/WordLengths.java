2708617c99cce0accb80459d455371b51f9fe81d
Back2RL <leonardoertelt@gmail.de>
2015-12-06 17:14:10 +0100

    added missing content PR1

--- PRuebungen/ueb5/01/WordLengths.java
+++ PRuebungen/ueb5/01/WordLengths.java
@@ -7,6 +7,10 @@
  * Matrikelnummer 1276156
  * leonard.oertelt@stud.hs-hannover.de
  *
+ * Julian Opitz
+ * Matrikelnummer 1302082
+ * julian.opitz@stud.hs-hannover.de
+ *
  * -----------------------------------------
  * Programmbeschreibung
  */
