8653c64c36b12bab01203dfadc0f0b9452afada3
back2rl <leonardoertelt@web.de>
2016-03-30 15:29:28 +0200

    DSA Blatt1 fertig mit Quittung

--- Java/eclipse_workspace/Datenstrukturen_und_Algorithmen/src/A1_Maximale_Teilsumme/MaxTeilsumme.java
+++ Java/eclipse_workspace/Datenstrukturen_und_Algorithmen/src/A1_Maximale_Teilsumme/MaxTeilsumme.java
@@ -4,45 +4,47 @@ public class MaxTeilsumme {
 
 	public static void main(String[] args) {
 		//
-		int durchlaufe = 100;
+		int anzahlDurchlaufe = 100000000;
 		int anzahlZufallszahlen = 1000;
 
+		int maxSum = 0;
 		int[] array = { 31, -41, 59, 26, -53, 58, 97, -93, -23, 84 };
 
 		long start = System.currentTimeMillis();
-		for (int i = 0; i < durchlaufe; ++i) {
-			int maxSum1 = findeMaxTeilsumme1(array);
+
+		for (int i = 0; i < anzahlDurchlaufe; ++i) {
+			maxSum = findeMaxTeilsumme1(array);
+
+			{
+				// int[] randomArray =
+				// generiereZahlenfolge(anzahlZufallszahlen); // 10*n+1
+				{
+					// maxSum = findeMaxTeilsumme1(randomArray);
+					// 4.5*n^3+2*n^2+3*n+6
+					// in Summe pro Schleife: 4.5*n^3 + 2*n^2 + 13*n + 7
+				}
+				{
+					// maxSum = findeMaxTeilsumme2(randomArray);
+					// 9*n^2+4*n+6
+					// in Summe pro Schleife: 9*n^2 + 13*n + 7
+				}
+			}
+
+			// System.out.println("Maximale Teilsumme = " + maxSum);
 		}
 		double laufzeit = laufzeitSekunden(start);
 		System.out.println(" Laufzeit = " + laufzeit + " Sekunden");
-		System.out.println("Im Mittel benötigte die Berechnung " + laufzeit / durchlaufe + " Sekunden.");
-
-		start = System.currentTimeMillis();
-		for (int i = 0; i < durchlaufe; ++i) {
-			int[] randomArray = generiereZahlenfolge(anzahlZufallszahlen); // 10*n+1
-			int maxSum1 = findeMaxTeilsumme1(randomArray); // 4.5*n^3+2*n^2+3*n+6
-			// System.out.println("Die Berechnung der Summe " + maxSum1);
-
-			// in Summe pro Schleife: 4.5*n^3 + 2*n^2 + 13*n + 7
-		}
-		laufzeit = laufzeitSekunden(start);
-		System.out.println(" Laufzeit = " + laufzeit + " Sekunden");
-		System.out.println("Im Mittel benötigte die Berechnung " + laufzeit / durchlaufe + " Sekunden.");
-
-		start = System.currentTimeMillis();
-		for (int i = 0; i < durchlaufe; ++i) {
-			int[] randomArray = generiereZahlenfolge(anzahlZufallszahlen); // 10*n+1
-			int maxSum2 = findeMaxTeilsumme2(randomArray); // 9*n^2+4*n+6
-			// System.out.println("Die Berechnung der Summe " + maxSum2);
-
-			// in Summe pro Schleife: 9*n^2 + 13*n + 7
-		}
-		laufzeit = laufzeitSekunden(start);
-		System.out.println(" Laufzeit = " + laufzeit + " Sekunden");
-		System.out.println("Im Mittel benötigte die Berechnung " + laufzeit / durchlaufe + " Sekunden.");
+		System.out.println("Im Mittel benötigte die Berechnung " + laufzeit / anzahlDurchlaufe + " Sekunden.");
 
 	}
 
+	/**
+	 * Bruteforce Variante zum finden der maximalen Teilsumme in einem Array
+	 * 
+	 * @param array
+	 *            Array dessen maximale Teilsummer ermittelt werden soll
+	 * @return maximale Teilsumme im Array
+	 */
 	public static int findeMaxTeilsumme1(int[] array) {
 		int curstart = 0; // 1
 		int curend = array.length; // 1 + 1 = 2
@@ -69,6 +71,13 @@ public class MaxTeilsumme {
 		return cursum; // 1
 	}// -> 4.5 * array.length^3 + 2 * array.length^2 + 3 * array.length + 6
 
+	/**
+	 * Verbesserte Variante des Algorithmus zum Finden der maximalen Teilsumme
+	 * 
+	 * @param array
+	 *            Array dessen maximale Teilsummer ermittelt werden soll
+	 * @return maximale Teilsumme im Array
+	 */
 	public static int findeMaxTeilsumme2(int[] array) {
 		int curstart = 0; // 1
 		int curend = array.length; // 2
@@ -89,6 +98,14 @@ public class MaxTeilsumme {
 		return cursum; // 1
 	} // 9 * array.length^2 + 4 * array.length + 6
 
+	/**
+	 * Erstellt ein Array vom Typ Integer, welches mit Zufallszahlen von -1000
+	 * bis +1000 gefüllt wird
+	 * 
+	 * @param anzahl
+	 *            Anzahl der Arrayelemente
+	 * @return das gefüllte Array
+	 */
 	public static int[] generiereZahlenfolge(int anzahl) {
 		int[] array = new int[anzahl]; // anzahl
 		for (int i = 0; i < anzahl; ++i) { // 3 * anzahl
