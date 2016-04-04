ad05b10025fd919930c89df979c0bbccc34b11a8
Back2RL <leonardoertelt@gmail.de>
2016-04-04 15:30:28 +0200

    c++ try/catch

--- C/Cpp/vector/Vektor.cpp
+++ C/Cpp/vector/Vektor.cpp
@@ -1,83 +1,109 @@
-#include "Vektor.h"
+#include "Headers/Vektor.h"
 
 using namespace std;
 
 // die Memberfunktionen der Klasse Vektor
 // Konstruktoren (hat keinen return-Wert)
-Vektor::Vektor(const double &x, const double &y, const double &z)
-{
-	this->x = x;
-	this->y = y;
-	this->z = z;
+Vektor::Vektor(const double &x, const double &y, const double &z) {
+    this->x = x;
+    this->y = y;
+    this->z = z;
 }
+
 
 Vektor::Vektor(const Vektor &v) // copy-Konstruktor
 {
 //	x = v.getX();
 //	y = v.getY();
 //	z = v.getZ();
-	x = v.x;
-	y = v.y;
-	z = v.z;
+    x = v.x;
+    y = v.y;
+    z = v.z;
 }
 
-double Vektor::getX(void) const
-{
-	return x;
+double Vektor::getX(void) const {
+    return x;
 }
 
-void Vektor::setX(const double &x)
-{
-	this->x = x;
+void Vektor::setX(const double &x) {
+    this->x = x;
 }
 
-double Vektor::getY(void) const
-{
-	return y;
+double Vektor::getY(void) const {
+    return y;
 }
 
-void Vektor::setY(const double &y)
-{
-	this->y = y;
-}
-double Vektor::getZ(void) const
-{
-	return z;
+void Vektor::setY(const double &y) {
+    this->y = y;
 }
 
-void Vektor::setZ(const double &z)
-{
-	this->z = z;
+double Vektor::getZ(void) const {
+    return z;
+}
+
+void Vektor::setZ(const double &z) {
+    this->z = z;
 }
 
 double Vektor::length(void) const
 // const bedeutet: in der Memberfunktion werden keine Variablen geändert (nur Lesezugriff)
 {
-	return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+    return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+}
+
+double Vektor::dotProduct(const Vektor &v1, const Vektor &v2) {
+    return v1.getX() + v2.getX() + v1.getY() * v2.getY() + v1.getZ() * v2.getZ();
+}
+
+void Vektor::normalize(void) {
+    Vektor norm = getNormalized();
+    x = norm.getX();
+    y = norm.getY();
+    z = norm.getZ();
+}
+
+/*
+ * @return Vektor with length of 1.0
+ */
+Vektor Vektor::getNormalized(void) const {
+    if (!isZeroVector()) {
+        double len = length();
+        return Vektor(x, y, z) / len;
+    }
+    __throw_invalid_argument("Vektor is ZeroVector");
+    return Vektor(0, 0, 0);
+}
+
+bool Vektor::isZeroVector(void) const {
+    return length() <= SMALL_NUMBER;
 }
 
 
-Vektor operator+( const Vektor &a, const Vektor &b)
-{
-	return Vektor(a.x + b.x, a.y + b.y, a.z + b.z);
+Vektor operator+(const Vektor &a, const Vektor &b) {
+    return Vektor(a.x + b.x, a.y + b.y, a.z + b.z);
 }
 
-ostream& operator<<(ostream &os, const Vektor &a)
-{
-	os << "(" << a.x << "," << a.y << "," << a.z << ")";	
-	return os;
+ostream &operator<<(ostream &os, const Vektor &a) {
+    os << "(" << a.x << "," << a.y << "," << a.z << ")";
+    return os;
 }
 
-double operator*(const Vektor &a, const Vektor &b)
-{
-	return a.x * b.x + a.y * b.y + a.z * b.z;
+double operator*(const Vektor &a, const Vektor &b) {
+    return a.x * b.x + a.y * b.y + a.z * b.z;
 }
 
-Vektor operator*(const Vektor &a, const double &d)
-{
-	return Vektor(a.x * d, a.y * d, a.z * d);
+Vektor operator*(const Vektor &a, const double &d) {
+    return Vektor(a.x * d, a.y * d, a.z * d);
 }
-Vektor operator*(const double &d,const Vektor &a)
-{
-	return a * d;
+
+Vektor operator*(const double &d, const Vektor &a) {
+    return a * d;
+}
+
+Vektor operator/(const Vektor &a, const double &d) {
+    return Vektor(a.x / d, a.y / d, a.z / d);
+}
+
+Vektor operator/(const double &d, const Vektor &a) {
+    return a / d;
 }
--- C/Cpp/vector/vektoren.cpp
+++ C/Cpp/vector/vektoren.cpp
@@ -1,55 +1,72 @@
-#include <iostream>
-#include <iomanip>
-#include <cmath>
-
-#include "Vektor.h"
+#include <stdexcept>
+#include <cstdio>
+#include "Headers/Vektor.h"
 
 using namespace std;
 
 // Hauptprogramm
-int main (int argc, char *argv[])
-{
-	// ein Objekt der Klasse Vektor
-	Vektor v1; //1. Konstruktor verwendet (Standardwerte, also (0,0,0))
+int main(int argc, char *argv[]) {
+    // ein Objekt der Klasse Vektor
+    Vektor v1; //1. Konstruktor verwendet (Standardwerte, also (0,0,0))
 
-	v1.setX(1);
-	v1.setY(2);
-	v1.setZ(3);
+    v1.setX(1);
+    v1.setY(2);
+    v1.setZ(3);
 
-	cout.setf(ios::fixed);
-	cout.precision(3);
+    cout.setf(ios::fixed);
+    cout.precision(6);
 
 
-	double d = v1.length();
-	cout << "Länge von v1: " << d << endl;
-	
-	double x = v1.getX();
-	double y = v1.getY();
-	double z = v1.getZ();
-	cout << "Vektor v1 = (" << x <<"," << y << "," << z << ")" << endl;
+    double d = v1.length();
+    cout << "Länge von v1: " << d << endl;
 
-	Vektor v2 = Vektor(5,7,8); //1. Konstruktor verwendet
-	d = v2.length();
-	cout << "Länge von v2: " << d << endl;
+    double x = v1.getX();
+    double y = v1.getY();
+    double z = v1.getZ();
+    cout << "Vektor v1 = (" << x << "," << y << "," << z << ")" << endl;
 
-	Vektor v3 = v1; // 2. Konstruktor copy verwendet
-	d = v3.length();
-	cout << "Länge von v3: " << d << endl;
-	
-	Vektor v4 = v2 + v1;
-	d = v4.length();
-	cout << "Länge von v4: " << d << endl;
-	
-	double a = v2 * v1;
+    Vektor v2 = Vektor(5, 7, 8); //1. Konstruktor verwendet
+    d = v2.length();
+    cout << "Länge von v2: " << d << endl;
+
+    Vektor v3 = v1; // 2. Konstruktor copy verwendet
+    d = v3.length();
+    cout << "Länge von v3: " << d << endl;
+
+    Vektor v4 = v2 + v1;
+    d = v4.length();
+    cout << "Länge von v4: " << d << endl;
+
+    double a = v2 * v1;
 //	Vektor v5 = v1 * 7;
-	Vektor v5 = 7 * v1;
+    Vektor v5 = 7 * v1;
 
-	cout << "v1: " << v1 << endl;
-	cout << "v2: " << v2 << endl;
-	cout << "v3: " << v3 << endl;
-	cout << "v4: " << v4 << endl;
-	cout << "v5: " << v5 << endl;
-	cout << "v2 * v1 =  " << a << endl;
-	
-	return 0;
+    cout << "v1: " << v1 << endl;
+    cout << "v2: " << v2 << endl;
+    cout << "v3: " << v3 << endl;
+    cout << "v4: " << v4 << endl;
+    cout << "v5: " << v5 << endl;
+
+    cout << "v2 * v1 =  " << a << endl;
+    cout << "(v2) normalized =  " << v2.getNormalized() << endl;
+    v2.normalize();
+    cout << "(v2) normalized =  " << v2 << endl;
+    cout << "(v1) normalized =  " << v1.getNormalized() << endl;
+    cout << "v1 dot v2 =  " << Vektor::dotProduct(v1.getNormalized(), v2) << endl;
+
+    Vektor zero;
+    try {
+        cout << "zero normalized =  " << zero.getNormalized() << endl;
+    } catch (invalid_argument error) {
+        printf("zero : %s\n", error.what());
+    }
+    int* numbers = nullptr;
+    try {
+        int* numbers = new int[50];
+    } catch (bad_alloc e) {
+        printf("numbers : %s\n", e.what());
+    }
+
+    delete[] numbers;
+    return 0;
 }
