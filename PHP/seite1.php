8b78b651e595c89cad3335a2d33841579311fe10
Back2RL <leonardoertelt@gmail.de>
2016-04-27 11:31:25 +0200

    added gitignore (reverted from commit 4ed95d848785b443e211c494fb77abef91e8be3c) added php examples and parts of DSA 5

---
+++ PHP/seite1.php
@@ -0,0 +1,55 @@
+<form action="HelloWorld.php" method="post">
+Vorname: <input type="text" name="vorname" /><br />
+Namename: <input type="text" name="nachname" /><br />
+<input type="Submit" value="Absenden" />
+</form>
+
+<form action="HelloWorld.php?wochentag=Sonntag" method="post">
+Vorname: <input type="text" name="vorname" /><br />
+Namename: <input type="text" name="nachname" /><br />
+<input type="Submit" value="Absenden" />
+</form>
+
+<form action="HelloWorld.php" method="post">
+<input type="Password" name="passwort" />
+<input type="Submit" value="Absenden" />
+</form>
+
+<form action="HelloWorld.php" method="post">
+Username:<br>
+<input type="Text" name="username" /><br />
+Passwort:<br />
+<input type="Password" name="passwort" /><br />
+<input type="Submit" value="Absenden" /><br />
+</form>
+
+<form action="HelloWorld.php" method="post">
+E-Mail:<br>
+<input type="Text" name="email"><br><br>
+ 
+Kommentar:<br>
+<textarea name="kommentar" cols="30" rows="5">
+</textarea>
+ 
+<input type="Submit" value="Absenden">
+</form>
+
+<form action="HelloWorld.php?seite=start" method="post">
+<input type="Submit" value="Start" />
+</form>
+
+
+<form action="HelloWorld.php" method="post">
+Registrierung:<br>
+Name: <input type="text" name="name" /><br />
+E-Mail: <input type="text" name="email" /><br />
+Passwort: <input type="password" name="passwort" /><br />
+<input type="submit" value="Registrieren" />
+</form>
+
+<form action="HelloWorld.php" method="post">
+Login:<br>
+E-Mail: <input type="text" name="email" /><br />
+Passwort: <input type="password" name="passwort" /><br />
+<input type="submit" value="Login" />
+</form>
