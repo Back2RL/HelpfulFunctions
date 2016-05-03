1c2d2849547d1ef34ffb5f5e8f263bbc21f38661
Back2RL <leonardoertelt@web.de>
2016-05-03 12:18:02 +0200

    DSA 6 SinglylinkedList

---
+++ Java/eclipse_workspace/Datenstrukturen_und_Algorithmen/src/Aufgabe_6/ListTest.java
@@ -0,0 +1,11 @@
+package Aufgabe_6;
+
+public class ListTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SinglyLinkedList liste = SinglyLinkedList.generateRandomListOfLength(10);
+		System.out.println(liste);
+	}
+
+}
