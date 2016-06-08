81661f2b9d7599bf441362ead4644d8a29a39561
back2rl <leonardoertelt@web.de>
2016-06-08 17:42:39 +0200

    added progress displaying to swing, TODO: find error

--- Java/eclipse_workspace/Programmieren_2/src/SwingExamples/SwingTest.java
+++ Java/eclipse_workspace/Programmieren_2/src/SwingExamples/SwingTest.java
@@ -2,15 +2,20 @@ package SwingExamples;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Map;
+import java.util.TreeMap;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class SwingTest extends JFrame {
 
@@ -25,11 +30,16 @@ public class SwingTest extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 		final JLabel yellowLabel = new JLabel("Hello World");
+		final JLabel anotherLabel = new JLabel("another");
 		yellowLabel.setOpaque(true);
 		yellowLabel.setBackground(Color.white);
 		yellowLabel.setPreferredSize(new Dimension(200, 80));
+		final Container test = new JPanel();
+		test.setLayout(new BoxLayout(test, BoxLayout.Y_AXIS));
+		getContentPane().add(test, BorderLayout.WEST);
 
-		getContentPane().add(yellowLabel, BorderLayout.CENTER);
+		test.add(yellowLabel, BoxLayout.X_AXIS);
+		test.add(anotherLabel, BoxLayout.X_AXIS);
 		final JButton button = new JButton("paff");
 		getContentPane().add(button, BorderLayout.NORTH);
 		final JButton button2 = new JButton("poff");
@@ -47,41 +57,77 @@ public class SwingTest extends JFrame {
 		button.addActionListener(listener);
 		button2.addActionListener(new ActionListener() {
 			private int counter;
-			private int activeActions;
+			private final Map<Integer, Integer> progess = new TreeMap<>();
 			private String text;
 
-			synchronized private int addAction(final int i) {
-				activeActions = Math.max(0, activeActions + i);
-				return activeActions;
+			synchronized private int createProgressTask() {
+				progess.put(progess.size(), 0);
+				return progess.size() - 1;
+			}
+
+			synchronized private int getAvaerageProgress() {
+				int sum = 0;
+				for (final Integer value : progess.values()) {
+					sum += value;
+				}
+				return sum / getProgressTasks();
+			}
+
+			synchronized private int getProgressTasks() {
+				return progess.size();
+			}
+
+			synchronized private void setProgress(final int index, final int value) {
+				progess.replace(index, value);
+			}
+
+			synchronized private void removeProgressTask(final int index) {
+				progess.remove(index);
 			}
 
 			@Override
 			public void actionPerformed(final ActionEvent e) {
-				addAction(1);
+				final int myIndex = createProgressTask();
 				// button pressed
 				// show user that processing occurs
-				setCursor(new Cursor(Cursor.WAIT_CURSOR));
+				if (getProgressTasks() == 0) {
+					setCursor(new Cursor(Cursor.WAIT_CURSOR));
+				}
 				// background thread for the heavy duty
 				final Thread ht = new Thread() {
 					@Override
 					synchronized public void run() {
-						// the heavy duty
-						for (long i = 0; i < 10_000_000_000L; ++i) {
-						}
 						if (text == null) {
 							text = button2.getText();
 						}
+						// the heavy duty
+						final long max = 1_000_000_000L;
+						final long showProgress = max / 100;
+						for (long i = 0; i < max; ++i) {
+							if (i % showProgress == 0L) {
+								setProgress(myIndex, (int) ((i * 100) / max));
+
+								EventQueue.invokeLater(new Runnable() {
+									@Override
+									public void run() {
+										button2.setText(text + " (" + getAvaerageProgress() + " %)");
+									}
+								});
+							}
+
+						}
+
 						counter++;
 						// end heavy duty
 						// new thread to change swing elements, needed for
 						// thread-safety
+						removeProgressTask(myIndex);
 						EventQueue.invokeLater(new Runnable() {
 							@Override
 							public void run() {
 								button2.setText(text + " " + counter);
 								yellowLabel.setText(yellowLabel.getText() + " -");
-
-								if (addAction(-1) < 1) {
+								if (getProgressTasks() < 1) {
 									setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 								}
 							}
