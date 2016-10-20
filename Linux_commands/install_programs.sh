8ac61ea8cfc115e8e9f75ba5a3cb41fbbec8d52f
back2rl <leonardoertelt@gmail.com>
2016-10-20 11:40:42 +0200

    added java installer

--- Linux_commands/install_programs.sh
+++ Linux_commands/install_programs.sh
@@ -6,4 +6,6 @@ sudo apt-get install vim
 #/etc/preload.conf (settings)
 sudo apt-get install htop
 sudo apt-get install cpufrequtils
-
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
