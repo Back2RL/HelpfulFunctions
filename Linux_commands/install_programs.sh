d229f96e534c92d3a77b5647700b1462250d9a5c
back2rl <leonardoertelt@gmail.com>
2016-10-26 14:43:30 +0200

    added gthump, added psensor

--- Linux_commands/install_programs.sh
+++ Linux_commands/install_programs.sh
@@ -2,10 +2,16 @@
 sudo apt-get update
 sudo apt-get install g++
 sudo apt-get install vim
-#sudo apt-get install preload
+sudo apt-get install preload
+sudo apt-get install gthumb
 #/etc/preload.conf (settings)
 sudo apt-get install htop
 sudo apt-get install cpufrequtils
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
+
+sudo add-apt-repository ppa:jfi/ppa
+sudo apt-get update
+sudo apt-get install psensor
+sudo sensors-detect
