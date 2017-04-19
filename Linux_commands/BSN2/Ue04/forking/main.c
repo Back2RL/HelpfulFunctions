88bb3183fbf5e634c619f9cbd5a0b09081b22d75
back2rl <leonardoertelt@gmail.com>
2017-04-19 12:42:24 +0200

    finished BSN2 Ue04

--- Linux_commands/BSN2/Ue04/forking/main.c
+++ Linux_commands/BSN2/Ue04/forking/main.c
@@ -5,22 +5,38 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
+// Ue04 Aufg. 3
 void simpleFork();
 
 void checkChildExitStatus(int status, int childPID);
 
+
+// Ue04 Aufg. 4a
 void chain(int numChilds);
 
+
+// Ue04 Aufg. 4e
 void singleParent(int numChilds);
 
+
+// Ue04 Aufg. 5
 void childParentWriting();
+
+// Ue04 Aufg. 6a
+void forkExec(int argc, char *args[]);
+
+// Ue04 Aufg. 6c
+void alternative_system(int argc, char *args[]);
 
 int main(int argc, char *args[]) {
 
 	//simpleFork();
 	//chain(50);
 	//singleParent(50);
-	childParentWriting();
+	//childParentWriting();
+
+	forkExec(argc, args);
+	//alternative_system(argc, args);
 
 	return 0;
 }
@@ -275,5 +291,112 @@ void childParentWriting() {
 	errno = 0;
 	if (close(fileDescriptor) == -1 || errno != 0) {
 		perror("Fehler beim Schließen von abc.txt");
+	}
+}
+
+
+void forkExec(int argc, char *args[]) {
+
+	int index;
+	int *childs;
+
+	for (int i = 0; i < argc; ++i) {
+		printf("Argument %d : %s\n", i, args[i]);
+	}
+
+	if (argc < 2) {
+		return;
+	}
+
+	// Speicher holen für die List von Child-PIDs
+	errno = 0;
+	childs = (int *) malloc((argc - 1) * sizeof(int));
+	if (childs == NULL || errno != 0) {
+		perror("Fehler beim Allokieren von Speicher");
+		return;
+	}
+
+	for (index = 1; index < argc; ++index) {
+		pid_t pid;
+		errno = 0;
+		pid = fork();
+		if (pid == -1 || errno != 0) {
+			perror("Fehler beim forking");
+			printf("return code of fork: %d\n", pid);
+			printf("Child pid: %d\n", getpid());
+			printf("Childs ppid: %d\n", getppid());
+		}
+		if (pid == 0) {
+			// Child
+			printf("Kind:\n");
+			printf("Kind: chain number: %d\n", index + 1);
+			printf("Kind: return code of fork: %d\n", pid);
+			printf("Kind: pid: %d\n", getpid());
+			printf("Kind: ppid: %d\n", getppid());
+
+			// exec hier
+			errno = 0;
+			if (execlp(args[index], args[index], NULL) == -1 || errno != 0) {
+				printf("exec on \"%s\" failed.\n", args[index]);
+				perror("Fehler beim exec");
+			}
+
+			break;
+		} else {
+			// Parent
+			int childPID = pid;
+			childs[index - 1] = childPID;
+
+			printf("Parent:\n");
+			printf("Parent: chain number: %d\n", index);
+			printf("Parent: return code of fork: %d\n", pid);
+			printf("Parent: pid: %d\n", getpid());
+			printf("Parent: ppid: %d\n", getppid());
+
+			// wenn parent alle Kinder erzeugt hat wartet er auf deren Beendigung
+			if (index + 1 == argc) {
+				sleep(3);
+				for (int i = 0; i < argc - 1; ++i) {
+					int status;
+
+					errno = 0;
+					pid = waitpid(childs[i], &status, 0);
+					if (pid == -1 || errno != 0) {
+						perror("Fehler beim Warten auf Kindprozess");
+					} else {
+						checkChildExitStatus(status, childs[i]);
+					}
+				}
+			}
+		}
+	}
+
+	// alle Parent/Childs löschen den allokierten Speicher
+	free(childs);
+}
+
+
+void alternative_system(int argc, char *args[]) {
+
+	for (int i = 0; i < argc; ++i) {
+		printf("Argument %d : %s\n", i, args[i]);
+	}
+
+	if (argc < 2) {
+		return;
+	}
+
+	for (int index = 1; index < argc; ++index) {
+		int rc;
+		errno = 0;
+		rc = system(args[index]);
+		if (errno != 0) {
+			perror("Fehler beim Aufrufen von system(...)");
+		}
+		if (rc == -1) {
+			printf("Child process could not be created, or its status could  not  be retrieved");
+		} else if (rc != 0) {
+			checkChildExitStatus(rc, -1);
+		}
 	}
 }
