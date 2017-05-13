0d4964a780238417ebce1b699df0a4462dda8328
Back2RL <leonardoertelt@gmail.com>
2017-05-13 18:43:52 +0200

    update

--- SQL/AR/AR_Login.sql
+++ SQL/AR/AR_Login.sql
@@ -15,7 +15,9 @@ where ar.session.user = 1
 and ar.session.ended is null;
 
 -- create new session for user from userid, ip, sessionkey and current time
-INSERT INTO ar.session(id,ip,sessionkey,user,begin,updated,ended)VALUES(default,'127.0.0.1',md5('somerandomkey'),1,default,default,null);
+INSERT INTO ar.session(id,ip,sessionkey,user,begin,updated,ended)
+VALUES
+(default,'127.0.0.1',md5('somerandomkey'),1,default,default,null);
 
 
 commit;
