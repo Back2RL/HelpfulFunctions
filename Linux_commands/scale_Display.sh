ff331c4f530d20cded704b8ff2831e17ab48c6fe
Back2RL <leonardoertelt@gmail.com>
2017-09-05 18:04:58 +0200

    improved display scaling script

--- Linux_commands/scale_Display.sh
+++ Linux_commands/scale_Display.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-echo "Scaling Display 1.5"
 xrandr --current
-xrandr --output DP-1 --scale 1.5x1.5
+
+echo 10 seconds until changes will be applied
+
+ADAPTER=eDP1
+MODE=1366x768
+PANNING=1920x1080
+SCALE=1.0
+
+sleep 10
+xrandr --output "$ADAPTER" --mode "$MODE" --panning "$PANNING" --scale "$SCALE"x"$SCALE"
